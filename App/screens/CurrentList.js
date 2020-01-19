f3fbe24ab24b500360756fdffa10c81074ea116c
Simas <simas@simas-dei.com>
2020-01-19 05:20:55 +0200

    add async local storage

--- App/screens/CurrentList.js
+++ App/screens/CurrentList.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   FlatList,
   KeyboardAvoidingView,
   StyleSheet,
+  ActivityIndicator,
 } from 'react-native';
 import uuid from 'uuid/v4';
-
-import nachos from '../data/nachos';
+import AsyncStorate from '@react-native-community/async-storage';
 
 import ListItem from '../components/ListItem';
 import Separator from '../components/UI/Separator';
@@ -20,7 +20,12 @@ const styles = StyleSheet.create({
 });
 
 const CurrentList = () => {
-  const [list, setList] = useState(nachos);
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const updateStoredCurrentList = list => {
+    AsyncStorate.setItem('@@IngredientList/currentList', JSON.stringify(list));
+  };
 
   const onFavoritePress = () => alert('Ahoy sailor o/');
 
@@ -29,27 +34,47 @@ const CurrentList = () => {
       id: uuid(),
       name: text,
     };
-    setList([newIngredient, ...list]);
+    const newList = [newIngredient, ...list];
+    setList(newList);
+    updateStoredCurrentList(newList);
   };
 
   const onLeftSwipeHandler = () => {
     alert('Ahoy Sailor ⛵');
   };
 
-  const onRightSwipeHandler = () => {
-    alert('Alright alright alright 👌');
+  const onRightSwipeHandler = id => {
+    const newList = list.filter(item => item.id !== id);
+    setList(newList);
+    updateStoredCurrentList(newList);
   };
+
+  const ingredientListInit = async () => {
+    try {
+      const data = await AsyncStorate.getItem('@@IngredientList/currentList');
+      const parsedData = await JSON.parse(data);
+      if (parsedData) setList(parsedData);
+    } catch (error) {
+      console.log(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    ingredientListInit();
+  }, []);
 
   const renderNachos = () => (
     <FlatList
       data={list}
-      renderItem={({ item: { name }, index }) => (
+      renderItem={({ item: { name, id }, index }) => (
         <ListItem
           name={name}
           onFavoritePress={onFavoritePress}
           isFavorite={index % 2 === 0}
           onLeftSwipeHandler={onLeftSwipeHandler}
-          onRightSwipeHandler={onRightSwipeHandler}
+          onRightSwipeHandler={() => onRightSwipeHandler(id)}
         />
       )}
       keyExtractor={({ id }) => id}
@@ -57,6 +82,13 @@ const CurrentList = () => {
       ListHeaderComponent={() => <AddItem onSubmitEditing={onSubmitHandler} />}
     />
   );
+
+  if (loading)
+    return (
+      <SafeAreaView size="large">
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
 
   return (
     <SafeAreaView style={styles.listContainer}>
