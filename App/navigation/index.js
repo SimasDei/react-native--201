24e076faae81e869c8f57af593d80273d6dcb497
Simas <simas@simas-dei.com>
2020-01-20 07:25:20 +0200

    add tab navigation

--- App/navigation/index.js
+++ App/navigation/index.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import { Image, Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import CurrentList from '../screens/CurrentList';
 import ItemDetails from '../screens/ItemDetails';
+
+import FavoritesList from '../screens/Favorites';
 
 const CurrentListStack = createStackNavigator(
   {
@@ -24,4 +29,64 @@ const CurrentListStack = createStackNavigator(
   },
 );
 
-export default createAppContainer(CurrentListStack);
+const FavoritesListStack = createStackNavigator({
+  FavoritesList: {
+    screen: FavoritesList,
+    navigationOptions: {
+      headerTitle: 'Favorites',
+    },
+  },
+});
+
+const Tabs = createBottomTabNavigator(
+  {
+    CurrentList: {
+      screen: CurrentListStack,
+      navigationOptions: {
+        tabBarLabel: 'Current',
+      },
+    },
+    FavoritesList: {
+      screen: FavoritesListStack,
+      navigationOptions: {
+        tabBarLabel: 'Favorites',
+      },
+    },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor, focused }) => {
+        const { routeName } = navigation.state;
+        let image;
+
+        if (routeName === 'CurrentList')
+          image = Platform.select({
+            ios: require('../assets/icons/ios-list.png'),
+            android: require('../assets/icons/md-list.png'),
+          });
+        else {
+          if (focused)
+            image = Platform.select({
+              ios: require('../assets/icons/ios-star.png'),
+              android: require('../assets/icons/md-star.png'),
+            });
+          else
+            image = Platform.select({
+              ios: require('../assets/icons/ios-star-outline.png'),
+              android: require('../assets/icons/md-star-outline.png'),
+            });
+        }
+
+        return (
+          <Image
+            source={image}
+            resizeMode="contain"
+            style={{ width: 25, tintColor }}
+          />
+        );
+      },
+    }),
+  },
+);
+
+export default createAppContainer(Tabs);
